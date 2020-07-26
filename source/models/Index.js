7ca3ed094d358a434ac93044b813f5df33b52f71
Andrew McPherson <ehgoodenough@gmail.com>
2020-07-26 07:39:02 -0700

    Added bomb dropping.

--- source/models/Index.js
+++ source/models/Index.js
@@ -11,13 +11,12 @@ const CENTER_POSITION = {"x": 0., "y": 0}
 const OTHER_POSITION = {"x": TILE * 5, "y": TILE * 2}
 
 class Player {
-    constructor () {
-        this.key = shortid.generate()
+    constructor() {
+        this.type = "player"
         this.position = {"x": 100, "y": 100}
         this.image = require("assets/images/red-monkey.png")
-        this.type = "player"
 
-        this.speed = 48
+        this.speed = TILE * 8
     }
     update(delta) {
         if(Keyb.isPressed("<left>")) {
@@ -32,6 +31,41 @@ class Player {
         if(Keyb.isPressed("<down>")) {
             this.position.y += this.speed * delta.s
         }
+
+        const position = {}
+        position.x = Math.round(this.position.x / TILE) * TILE
+        position.y = Math.round(this.position.y / TILE) * TILE
+        position.key = Point.toString(position)
+
+        if(Keyb.wasJustPressed("<space>", delta.ms)
+        && this.collection.has(position.key) == false) {
+            this.collection.add(new Bomb({"position": position, "distance": 3}))
+        }
+    }
+}
+
+class Point {
+    // constructor({x, y}) {
+    //     this.x = x
+    //     this.y = y
+    // }
+    static toString(point) {
+        return point.x + "x" + point.y
+    }
+}
+
+class Bomb {
+    constructor({position, distance}) {
+        this.type = "bomb"
+        this.position = position
+        this.image = require("assets/images/bomb1.png")
+
+        this.key = this.position.key || Point.toString(this.position)
+
+        this.time = 0
+    }
+    update(delta) {
+        // console.log(delta)
     }
 }
 
@@ -42,6 +76,14 @@ class Collection {
     add(value) {
         value.key = value.key || shortid.generate()
         this.values[value.key] = value
+        value.collection = this
+    }
+    has(key) {
+        return !!this.values[key]
+    }
+    remove(value) {
+        this.values[value.key].collection = undefined
+        delete this.values[value.key]
     }
     get(key) {
         if(key == undefined) {
@@ -65,29 +107,31 @@ export default class Index {
 
         this.entities.add(new Player())
 
-        // for(let x = -36; x <= 36; x += 1) {
-        //     for(let y = -20; y <= 20; y += 1) {
-        //         const block = {
-        //             "type": "block",
-        //             "key": shortid.generate(),
-        //             "position": {"x": x * 16, "y": y * 16},
-        //             "image": require("assets/images/wall.png"),
-        //         }
-        //         if(x % 2 == 0 || y % 2 == 0) {
-        //             block.image = require("assets/images/crate.png")
-        //             continue // DEBUG
-        //         }
-        //         this.entities.add(block)
-        //     }
-        // }
+        const height = 5// 20
+        const width = 5 // 36
+        for(let x = -width; x <= width; x += 1) {
+            for(let y = -height; y <= height; y += 1) {
+                const block = {
+                    "type": "block",
+                    "key": shortid.generate(),
+                    "position": {"x": x * 16, "y": y * 16},
+                    "image": require("assets/images/wall.png"),
+                }
+                if(x % 2 == 0 || y % 2 == 0) {
+                    block.image = require("assets/images/crate.png")
+                    continue // DEBUG
+                }
+                this.entities.add(block)
+            }
+        }
 
         this.play()
     }
     update(delta) {
-        if(Keyb.wasJustPressed("<space>", delta.ms)) {
-            // Director.flush()
-            this.play()
-        }
+        // if(Keyb.wasJustPressed("<space>", delta.ms)) {
+        //     // Director.flush()
+        //     this.play()
+        // }
 
         this.entities.get().forEach((entity) => {
             if(entity.update instanceof Function) {
